02b55d7067d443101fd71fdc675cfd9bc58d15cb
rnegash <rnegash@macbookproc02w80gehv2g.berlin.internal.babbel.com>
2018-05-23 15:03:46 +0200

    input functioning

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ButtonPanel from "./ButtonPanel.js";
+import Button from "./Button.js";
 import Display from "./Display.js";
 
 export default class Calculator extends React.Component {
@@ -8,11 +8,13 @@ export default class Calculator extends React.Component {
 
     this.state = { inputValue: "" };
 
-    this.onClick = this.onClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  onClick(e) {
-    this.setState({ inputValue: this.props.sign }, function() {
+  handleClick(value) {
+    this.setState({ inputValue: value }, function() {
+      //console.log(this.state.inputValue+' hi '+this.props.sign);
+      console.log(value);
       console.log(this.state.inputValue);
     });
   }
@@ -21,7 +23,25 @@ export default class Calculator extends React.Component {
     return (
       <div className="calculator">
         <Display />
-        <ButtonPanel onClick={this.props.onClick} />
+        <Button handleClick={this.handleClick} sign="AC"  />
+        <Button handleClick={this.handleClick} sign="+/-" />
+        <Button handleClick={this.handleClick} sign="%" />
+        <Button handleClick={this.handleClick} sign="/" classes="orange" /> <br />
+        <Button handleClick={this.handleClick} sign="7" />
+        <Button handleClick={this.handleClick} sign="8" />
+        <Button handleClick={this.handleClick} sign="9" />
+        <Button handleClick={this.handleClick} sign="*" classes="orange" /> <br />
+        <Button handleClick={this.handleClick} sign="4" />
+        <Button handleClick={this.handleClick} sign="5" />
+        <Button handleClick={this.handleClick} sign="6" />
+        <Button handleClick={this.handleClick} sign="-" classes="orange" /> <br />
+        <Button handleClick={this.handleClick} sign="1" />
+        <Button handleClick={this.handleClick} sign="2" />
+        <Button handleClick={this.handleClick} sign="3" />
+        <Button handleClick={this.handleClick} sign="+" classes="orange" /> <br />
+        <Button handleClick={this.handleClick} sign="0" classes="button-50"/>
+        <Button handleClick={this.handleClick} sign="," />
+        <Button handleClick={this.handleClick} sign="=" classes="orange" /> <br />
       </div>
     );
   }
