cb64bf06fcd97c966ddaa8164cc3f142a2bc752d
LinaLtu <lsuodyte@babbel.com>
2018-05-24 14:59:12 +0200

    Added Logic for Showing the Correct Result

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export default class Display extends React.Component {
-    render() {
-        return (
-            <div className="output">
-                <div>{this.props.number}</div>
-                <div>{this.props.operator}</div>
-                <div>{this.props.inputValue}</div>
-            </div>
-        );
+  render() {
+    let displayState;
+    if (this.props.number === 0 && this.props.inputValue != 0) {
+      displayState = this.props.inputValue;
+    } else if (this.props.number != 0 && this.props.inputValue != 0) {
+      displayState = this.props.inputValue;
+    } else if (this.props.number != 0 && this.props.inputValue === 0) {
+      displayState = this.props.number;
+    } else {
+      displayState = 0;
     }
+    return <div className="output">{displayState}</div>;
+  }
 }
